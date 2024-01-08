4ff8fc42159ae70045a7258563274968ef5f5e26
Aleksandr Zhukov <aleksandrzhukov@00008809s-MBP.lan>
2024-01-08 03:30:38 +0100

    add gh repo link

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Link } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 import "./App.css";
 import ChordsSheet from "./components/ChordsSheet";
 
@@ -7,6 +8,17 @@ function App() {
     <ChakraProvider>
       <div className="App">
         <ChordsSheet />
+        <Link
+          sx={{
+            position: "absolute",
+            top: "0.5rem",
+            right: "0.5rem",
+          }}
+          href="https://github.com/nogizhopaboroda/balalaika-chords-sheet-generator"
+          isExternal
+        >
+          Github Repo <ExternalLinkIcon mx="2px" />
+        </Link>
       </div>
     </ChakraProvider>
   );
