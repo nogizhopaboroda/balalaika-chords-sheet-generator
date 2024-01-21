4964514acc26613180a13984d6c2c5c2511c1879
Aleksandr Zhukov <aleksandrzhukov@00008809s-MBP.lan>
2024-01-21 01:16:04 +0100

    extract chord wrapper

--- src/components/ChordsSheet/ChordGroup.tsx
+++ src/components/ChordsSheet/ChordGroup.tsx
@@ -1,59 +1,11 @@
-import { Box, Divider, Flex } from "@chakra-ui/react";
-import { useState } from "react";
-import type { ChordData, ChordGroup, ChordTuning } from "../../types";
-import { getFret, pickVariant } from "../../helpers";
-import Chord from "./Chord";
+import { Box } from "@chakra-ui/react";
+import type { ChordGroup, ChordTuning } from "../../types";
+import ChordWrapper from "./ChordWrapper";
 
 interface ChordGroupComponentProps {
   group: ChordGroup;
   tuning?: ChordTuning;
 }
-
-const ChordWrapper = ({
-  chord,
-  tuning,
-}: {
-  chord: ChordData;
-  tuning?: ChordTuning;
-}) => {
-  const [isExpanded, setIsExpanded] = useState(false);
-  const variant = pickVariant(chord.variants);
-  if (!variant) {
-    return null;
-  }
-  if (isExpanded) {
-    return (
-      <>
-        <Flex
-          sx={{
-            cursor: "pointer",
-          }}
-          onClick={() => setIsExpanded((val) => !val)}
-        >
-          {chord.variants.map((variant, index) => (
-            <Chord
-              key={index}
-              chord={variant}
-              title={variant.title ?? chord.title}
-              tuning={tuning}
-            />
-          ))}
-        </Flex>
-        <Divider orientation="vertical" />
-      </>
-    );
-  }
-  return (
-    <Box
-      sx={{
-        cursor: "pointer",
-      }}
-      onClick={() => setIsExpanded((val) => !val)}
-    >
-      <Chord chord={variant} title={chord.title} tuning={tuning} />
-    </Box>
-  );
-};
 
 const ChordGroupComponent = ({ group, tuning }: ChordGroupComponentProps) => (
   <Box
---
+++ src/components/ChordsSheet/ChordWrapper.tsx
@@ -0,0 +1,53 @@
+import { Box, Divider, Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import { pickVariant } from "../../helpers";
+import type { ChordData, ChordTuning } from "../../types";
+import Chord from "./Chord";
+
+const ChordWrapper = ({
+  chord,
+  tuning,
+}: {
+  chord: ChordData;
+  tuning?: ChordTuning;
+}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const variant = pickVariant(chord.variants);
+  if (!variant) {
+    return null;
+  }
+  if (isExpanded) {
+    return (
+      <>
+        <Flex
+          sx={{
+            cursor: "pointer",
+          }}
+          onClick={() => setIsExpanded((val) => !val)}
+        >
+          {chord.variants.map((variant, index) => (
+            <Chord
+              key={index}
+              chord={variant}
+              title={variant.title ?? chord.title}
+              tuning={tuning}
+            />
+          ))}
+        </Flex>
+        <Divider orientation="vertical" />
+      </>
+    );
+  }
+  return (
+    <Box
+      sx={{
+        cursor: "pointer",
+      }}
+      onClick={() => setIsExpanded((val) => !val)}
+    >
+      <Chord chord={variant} title={chord.title} tuning={tuning} />
+    </Box>
+  );
+};
+
+export default ChordWrapper;
